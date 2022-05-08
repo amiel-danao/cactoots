f125fd615c6e2b3a000bd661d42529cf34ed293c
Amiel <amielrenaissance4@gmail.com>
2022-05-08 22:23:56 +0800

    added login

---
+++ app/src/main/java/com/thesis/cactoots/java/models/Item.java
@@ -0,0 +1,32 @@
+package com.thesis.cactoots.java.models;
+
+import java.util.Date;
+import java.util.Map;
+
+public class Item {
+    private String id;
+    private String name;
+    private Map<String, Integer> price;
+    private Map<String, Integer> quantity;
+    private Date timestamp;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<String, Integer> getPrice() {
+        return price;
+    }
+
+    public Map<String, Integer> getQuantity() {
+        return quantity;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+}
